4bdfb259bda3154d145d969dd711930f36a8c1d0
wizby0@naver.com <>
2019-01-23 17:23:07 +0900

    sourcecode upload

---
+++ blog/src/main/java/com/wooju/github/blog/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.wooju.github.blog.repositories;
+
+import com.wooju.github.blog.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
